3a4a3046c220b7001ede631533be4182788568b8
thefu21 <thefu21@gmx.net>
2024-04-11 20:13:34 +0200

    Spotify Api testing

--- src/lib/components/FullScreenPlayer.svelte
+++ src/lib/components/FullScreenPlayer.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { nowPlaying } from '$lib/stores/nowPlaying-store.js';
+    import { nowPlaying } from '$lib/stores/stores.js';
 </script>
 
 <main class="h-screen grid grid-cols-9 grid-rows-12 bg-gray-900">
@@ -9,10 +9,10 @@
     <div class="flex justify-center items-center col-start-4 col-end-7 row-start-9">
         <p>{$nowPlaying.name}</p>
     </div>
-    <div class="flex justify-center items-center col-start-5 row-start-10">
+    <div class="flex justify-center items-center col-start-5 row-start-11">
         <button>play/pause</button>
     </div>
-    <div class="flex justify-center items-center col-start-3 col-end-8 row-start-11">
+    <div class="flex justify-center items-center col-start-3 col-end-8 row-start-10">
         <input value="10" class="h-1 w-full" type="range" disabled/>
     </div>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,61 @@
 <script>
-    import FullScreenPlayer from "$lib/components/FullScreenPlayer.svelte";
+    import {onMount} from "svelte";
+    import axios from "axios";
+
+    let searchValue;
+
+    const searchArtists = async (e) => {
+        e.preventDefault()
+        console.log(await axios.get("https://api.spotify.com/v1/search", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            params: {
+                q: searchValue,
+                type: "track"
+            }
+        }))
+    }
+
+    const playTrack = async () => {
+        try {
+            await axios.put("https://api.spotify.com/v1/me/player/play",
+                { uris: ['https://api.spotify.com/v1/tracks/1OOtq8tRnDM8kG2gqUPjAj'] },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
+            console.log("Track is now playing!");
+        } catch (error) {
+            console.error("Error playing track:", error);
+            // Handle error
+        }
+    };
+
+
+    onMount(() => {
+        window.onSpotifyWebPlaybackSDKReady = () => {
+            const player = new Spotify.Player({
+                name: 'Web Playback SDK Quick Start Player',
+                getOAuthToken: cb => { cb(token); },
+                volume: 0.5
+            });
+
+            player.connect()
+        }
+    })
 </script>
 
-<main class="text-white">
-    <FullScreenPlayer></FullScreenPlayer>
+<svelte:head>
+    <script src="https://sdk.scdn.co/spotify-player.js"></script>
+</svelte:head>
+
+
+<main class="">
+    <input bind:value={searchValue} type="text">
+    <p>ds</p>
+    <button class="bg-green-600" on:click={() => {playTrack(searchValue)}}>hallo</button>
+    <button id="togglePlay">Toggle Play</button>
 </main>
