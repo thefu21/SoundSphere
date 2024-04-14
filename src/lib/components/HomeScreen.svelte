9a4be3f0ee3467cfd887b0e46f8302ac953b59a0
thefu21 <thefu21@gmx.net>
2024-04-14 21:03:41 +0200

    feat: player state change + Song Length + auto Transfer playback

--- src/lib/components/HomeScreen.svelte
+++ src/lib/components/HomeScreen.svelte
@@ -6,21 +6,27 @@
     import { Switch } from "$lib/components/ui/switch/index.js"
     import { Button } from "$lib/components/ui/button/index.js"
     import { Progress } from "$lib/components/ui/progress/index.js"
-    import {AudioLines, CirclePause, Menu, RadioTower, Search, SkipBack, SkipForward} from 'lucide-svelte';
+    import {AudioLines, CirclePause, CirclePlay, Menu, RadioTower, Search, SkipBack, SkipForward} from 'lucide-svelte';
     import {Label} from '$lib/components/ui/label/index.js';
     import {Skeleton} from '$lib/components/ui/skeleton/index.js';
     import {AspectRatio} from '$lib/components/ui/aspect-ratio/index.js';
 
-
     const clientID = 'eed7eaff183d4604b08e9de07393fbdd';
+
+    let player;
+
+    let paused;
 
     let nowPlayingImageUrl;
     let nowPlayingObject;
-    let nowPlayingImageColor = undefined;
+    let nowPlayingImageColor;
+    let nowPlayingPosition;
+    let nowPlayingLength;
 
 
     onMount(() => {
         const fac = new FastAverageColor();
+
         window.onSpotifyWebPlaybackSDKReady = async function () {
             const authCode = new URLSearchParams(window.location.search).get(
                 "code"
@@ -30,12 +36,10 @@
                 window.history.replaceState({}, document.title, window.location.origin + window.location.pathname);
             }
 
-            const token = getAccessToken(clientID);
-
-            const player = new Spotify.Player({
+            player = new Spotify.Player({
                 name: 'SoundSphere',
                 getOAuthToken: cb => {
-                    cb(token);
+                    cb(getAccessToken(clientID));
                 },
                 volume: 0.5
             });
@@ -45,7 +49,7 @@
             const spotifyApiSearch = async (query, type) => {
                 return await axios.get("https://api.spotify.com/v1/search", {
                     headers: {
-                        Authorization: `Bearer ${token}`
+                        Authorization: `Bearer ${getAccessToken(clientID)}`
                     },
                     params: {
                         q: query,
@@ -66,55 +70,75 @@
             })
              */
 
-            const nowPlaying = async () => {
-                return await axios.get("https://api.spotify.com/v1/me/player", {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                })
-            }
+            setInterval(() => nowPlayingPosition += paused ? 0 : 300, 300);
 
-             nowPlaying().then(r => {
-                 nowPlayingObject = r.data;
-                 nowPlayingImageUrl = nowPlayingObject.item.album.images[0].url;
-                 console.log(nowPlayingObject);
-                 fac.getColorAsync(nowPlayingImageUrl).then(res => nowPlayingImageColor = res.hex)
-                }
-            );
+            player.addListener('player_state_changed', (r) => {
+                nowPlayingObject = r.track_window.current_track;
+                nowPlayingImageUrl = nowPlayingObject.album.images[2].url;
+                fac.getColorAsync(nowPlayingImageUrl).then(res => nowPlayingImageColor = res.hex);
+                paused = r.paused;
+                nowPlayingPosition = r.position;
+                nowPlayingLength = r.duration;
+            });
+
+            player.addListener('ready', ({ device_id }) => {
+                axios.put("https://api.spotify.com/v1/me/player",
+                    {
+                        "device_ids": [device_id]
+                    },
+                    {
+                        headers: {
+                            Authorization: `Bearer ${getAccessToken(clientID)}`,
+                            'Content-Type': 'application/json',
+                        }
+                    }
+                )
+            });
+
         }
     })
 </script>
 
 <main style={`background: linear-gradient(0deg, #171313 0%, ${nowPlayingImageColor || '#181414'} 100%);`} class="h-screen grid grid-cols-11 grid-rows-12">
     <div class="flex justify-center items-center row-start-1 row-end-3  col-start-0 col-end-2">
-        <Button class="bg-transparent hover:bg-transparent hover:text-gray-500 active:text-gray-800"><Menu size="32"/></Button>
+        <Button class="bg-transparent hover:bg-transparent hover:text-gray-400 active:text-[#181414]"><Menu size="32"/></Button>
     </div>
     <div class="flex justify-center items-center row-start-1 row-end-3  col-start-3 col-end-10">
-        <Input class="h-[24px]" placeholder="Search" type="text"><Search/></Input>
+        <Input class="h-[24px]" placeholder="Search" type="text"><Search color="#74747a"/></Input>
     </div>
     <div class="flex justify-center items-center row-start-1 row-end-3  col-start-10 col-end-12">
-        <Label for="spotifyRadioToggle"><RadioTower color="#3e9392"/></Label>
-        <Switch class="m-3" id="spotifyRadioToggle"></Switch>
-        <Label for="spotifyRadioToggle"><AudioLines color="#3e9392"/></Label>
+        <Label for="spotifyRadioToggle"><RadioTower color={`${nowPlayingImageColor === undefined ? '#74747a' : '#181414'}`}/></Label>
+        <Switch style={`background-color: ${nowPlayingImageColor === undefined ? '#74747a' : '#181414'}`} class="m-3" id="spotifyRadioToggle"></Switch>
+        <Label for="spotifyRadioToggle"><AudioLines color={`${nowPlayingImageColor === undefined ? '#74747a' : '#181414'}`}/></Label>
     </div>
     <div class="flex justify-center items-center row-start-3 row-end-9 col-start-5 col-end-8">
         <AspectRatio ratio={1}>
             <img id="nowPlayingImage" src={nowPlayingImageUrl} alt="nowPlayingImage">
         </AspectRatio>
     </div>
-    <div class="col-start-3 col-end-10 row-start-9 row-end-11 lg:pt-5">
+    <div class="col-start-3 col-end-10 row-start-9 row-end-11 lg:pt-7">
         {#if nowPlayingObject}
-            <h3 class="text-2xl text-white p-0">{nowPlayingObject.item.name}</h3>
-            <h4 class="text-xl text-gray-600 p-0">{nowPlayingObject.item.artists.map(artist => artist.name).join(', ')}</h4>
+            <h3 class="text-2xl text-white p-0">{nowPlayingObject.name}</h3>
+            <h4 class="text-xl text-gray-400 p-0">{nowPlayingObject.artists.map(artist => artist.name).join(', ')}</h4>
         {/if}
     </div>
     <div class="flex justify-center items-center col-start-3 col-end-10 row-start-10">
-        <Progress id="progress" class="h-2"></Progress>
+        <Progress value={nowPlayingPosition} max={nowPlayingLength} id="progress" class="h-2"></Progress>
+    </div>
+    <div class="flex items-end  col-start-3 col-end-4 row-start-10">
+        <p class="text-gray-400">{Math.floor((nowPlayingPosition/1000/60) << 0) + ":" + String(Math.floor((nowPlayingPosition/1000) % 60)).padStart(2, '0')}</p>
+    </div>
+    <div class="flex justify-end items-end col-start-9 col-end-10 row-start-10">
+        <p class="text-gray-400">{"-" + Math.floor(((nowPlayingLength-nowPlayingPosition)/1000/60) << 0) + ":" + String(Math.floor(((nowPlayingLength-nowPlayingPosition)/1000) % 60)).padStart(2, '0')}</p>
     </div>
     <div class="flex justify-center items-center col-start-5 col-end-8 row-start-11">
-        <Button class="bg-transparent hover:bg-transparent hover:text-gray-500 active:text-gray-800"><SkipBack size="48"/></Button>
-        <Button class="bg-transparent hover:bg-transparent hover:text-gray-500 active:text-gray-800"><CirclePause size="48"/></Button>
-        <Button class="bg-transparent hover:bg-transparent hover:text-gray-500 active:text-gray-800"><SkipForward size="48"/></Button>
+        <Button on:click={() => player.previousTrack()} class="bg-transparent hover:bg-transparent hover:text-gray-400 active:text-[#181414]"><SkipBack size="48"/></Button>
+        <Button on:click={() => {if(paused === undefined) player.activateElement(); else player.togglePlay()}} class="bg-transparent hover:bg-transparent hover:text-gray-400 active:text-[#181414]">
+            {#if paused} <CirclePlay size="48"/>
+                {:else} <CirclePause size="48"/>
+            {/if}
+        </Button>
+        <Button on:click={() => player.nextTrack()} class="bg-transparent hover:bg-transparent hover:text-gray-400 active:text-[#181414]"><SkipForward size="48"/></Button>
     </div>
 </main>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <svelte:head>
     <script src="https://sdk.scdn.co/spotify-player.js"></script>
     <script src="https://unpkg.com/fast-average-color/dist/index.browser.min.js"></script>
-    <title>Test</title>
+    <title>SoundSphere</title>
 </svelte:head>
 
 
