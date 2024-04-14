55e102d048f29d53e3df58a625051cb52064f9b2
thefu21 <thefu21@gmx.net>
2024-04-14 12:39:06 +0200

    feat: added metadata fetching

--- src/lib/components/HomeScreen.svelte
+++ src/lib/components/HomeScreen.svelte
@@ -6,7 +6,7 @@
     import { Switch } from "$lib/components/ui/switch/index.js"
     import { Button } from "$lib/components/ui/button/index.js"
     import { Progress } from "$lib/components/ui/progress/index.js"
-    import {AudioLines, CirclePause, RadioTower, Search, SkipBack, SkipForward} from 'lucide-svelte';
+    import {AudioLines, CirclePause, Menu, RadioTower, Search, SkipBack, SkipForward} from 'lucide-svelte';
     import {Label} from '$lib/components/ui/label/index.js';
     import {Skeleton} from '$lib/components/ui/skeleton/index.js';
     import {AspectRatio} from '$lib/components/ui/aspect-ratio/index.js';
@@ -14,7 +14,13 @@
 
     const clientID = 'eed7eaff183d4604b08e9de07393fbdd';
 
+    let nowPlayingImageUrl;
+    let nowPlayingObject;
+    let nowPlayingImageColor = undefined;
+
+
     onMount(() => {
+        const fac = new FastAverageColor();
         window.onSpotifyWebPlaybackSDKReady = async function () {
             const authCode = new URLSearchParams(window.location.search).get(
                 "code"
@@ -48,6 +54,7 @@
                 })
             }
 
+            /*
             spotifyApiSearch("keine ahnung", "track").then(result => {
                 result.data.tracks.items.forEach(track => {
                     console.log("Track Name:", track.name);
@@ -57,6 +64,23 @@
                     console.log("------------------------------------------");
                 })
             })
+             */
+
+            const nowPlaying = async () => {
+                return await axios.get("https://api.spotify.com/v1/me/player", {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+            }
+
+             nowPlaying().then(r => {
+                 nowPlayingObject = r.data;
+                 nowPlayingImageUrl = nowPlayingObject.item.album.images[0].url;
+                 console.log(nowPlayingObject);
+                 fac.getColorAsync(nowPlayingImageUrl).then(res => nowPlayingImageColor = res.hex)
+                }
+            );
         }
     })
 
@@ -65,7 +89,10 @@
 
 </script>
 
-<main class="h-screen grid grid-cols-11 grid-rows-12 bg-gray-900">
+<main style={`background-color: ${nowPlayingImageColor || '#181414'}`} class="h-screen grid grid-cols-11 grid-rows-12">
+    <div class="flex justify-center items-center row-start-1 row-end-3  col-start-0 col-end-2">
+        <Button class="bg-transparent hover:bg-transparent hover:text-gray-500 active:text-gray-800"><Menu size="32"/></Button>
+    </div>
     <div class="flex justify-center items-center row-start-1 row-end-3  col-start-3 col-end-10">
         <Input class="h-[24px]" placeholder="Search" type="text"><Search/></Input>
     </div>
@@ -76,12 +103,14 @@
     </div>
     <div class="flex justify-center items-center row-start-3 row-end-9 col-start-5 col-end-8">
         <AspectRatio ratio={1}>
-            <Skeleton class="w-full h-full"/>
+            <img id="nowPlayingImage" src={nowPlayingImageUrl} alt="nowPlayingImage">
         </AspectRatio>
     </div>
-    <div class="col-start-3 col-end-10 row-start-9 row-end-10 lg:pt-8">
-        <h3 class="text-2xl text-white">Title</h3>
-        <p class="text-xl text-gray-600">Artist</p>
+    <div class="col-start-3 col-end-10 row-start-9 row-end-11 lg:pt-5">
+        {#if nowPlayingObject}
+            <h3 class="text-2xl text-white p-0">{nowPlayingObject.item.name}</h3>
+            <h4 class="text-xl text-gray-600 p-0">{nowPlayingObject.item.artists.map(artist => artist.name).join(', ')}</h4>
+        {/if}
     </div>
     <div class="flex justify-center items-center col-start-3 col-end-10 row-start-10">
         <Progress id="progress" class="h-2"></Progress>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 
 <svelte:head>
     <script src="https://sdk.scdn.co/spotify-player.js"></script>
+    <script src="https://unpkg.com/fast-average-color/dist/index.browser.min.js"></script>
     <title>Test</title>
 </svelte:head>
 
